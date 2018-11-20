f34467284c6be5969e32f6c41b6d85eb34914e31
bareisho <tudoutasty@gmail.com>
2018-11-20 09:50:28 +0800

    open new window

--- wuxuhui/js/index.js
+++ wuxuhui/js/index.js
@@ -10,3 +10,42 @@
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+var eventUtil = {
+    addHanlder: function(element, type, handler) {
+        element.addEventListener(type, handler, false);
+    },
+    removeHander: function(element, type, handler) {
+        element.removeEventListener(type, handler, false);
+    }
+}
+var requestAjax = function(options, hanlder) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                hanlder(xhr.responseText);
+                xhr = null;
+            } else {
+                alert("error: " + xhr.status);
+            }
+        }
+    }
+    xhr.open(options.type, options.url, false);
+    xhr.send(options.data || null);
+    return xhr;
+}
+var openDiscuss = {
+    click: function(target) {
+        //debugger
+        window.open("./discussion.html");
+    },
+    bind: function() {
+        var target = document.querySelectorAll(".user-info");
+            length = target.length;
+        for(var i=length-1;i--;) {
+            var element = target[i];
+            eventUtil.addHanlder(element, "click", openDiscuss.click);
+        }
+    }
+}
+openDiscuss.bind();
